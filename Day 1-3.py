19d9813603a8604f96a19d54d45e4031a0bd298e
CodeWithAsh <ashishlewate@gmail.com>
2025-12-24 14:57:36 +0530

    Add files via upload

---
+++ Day 1-3.py
@@ -0,0 +1,8 @@
+#Take a number and check whether it is positive, negative, or zero
+num = int(input("Enter a Number:"))
+if num <=-1 :
+    print("negative")
+elif num >=1:
+    print("positve")
+elif num == 0 :
+    print("zero")
