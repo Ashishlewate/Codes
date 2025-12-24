b076fb9acafb31e5e45af97e65e2dbcec80b3ff9
CodeWithAsh <ashishlewate@gmail.com>
2025-12-24 14:49:10 +0530

    Add files via upload

---
+++ Day 1-2.py
@@ -0,0 +1,6 @@
+#Take a number and check whether it is odd or even
+num = int(input("Enter a number:"))
+if num % 2:
+    print("odd")
+else:
+    print("even")
---
+++ Day 1.py
@@ -0,0 +1,6 @@
+#Take two numbers and print their sum
+Num1 = int(input("enter a number:"))
+Num2 = int(input("enter a number:"))
+Sum = Num1+Num2
+print(Sum)
+           
