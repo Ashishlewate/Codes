c303447862884aa70b8232aa7ac7441c97c36558
CodeWithAsh <ashishlewate@gmail.com>
2025-12-24 17:38:40 +0530

    Add files via upload

---
+++ Day  1-8.py
@@ -0,0 +1,12 @@
+#Take marks and print grade:
+marks = int(input("Enter marks:"))
+if marks >= 90:
+    print( "Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+elif marks >= 95:
+    print("Grade A++")
+else:
+    print("Grade D")
