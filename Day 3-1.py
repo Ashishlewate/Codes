629a9f785fe8fc86eda525ea9692f7d8a3d4dd2e
CodeWithAsh <ashishlewate@gmail.com>
2025-12-26 16:07:59 +0530

    Add files via upload

---
+++ Day 3-1.py
@@ -0,0 +1,8 @@
+#Take length and breadth and check whether it is a square or rectangle
+len = int(input("Enter Length:"))
+Brth = int(input("Enter Breath:"))
+
+if len == Brth:
+    print("Square")
+elif len * Brth :
+    print("Rectangle")
